f9658cc41c5db29d51a8d2a44efefac74b8c74c1
Beke Ágnes <suli.beke.agnes@gmail.com>
2020-05-09 19:33:19 +0200

    Server config file

--- server/Program.cs
+++ server/Program.cs
@@ -6,7 +6,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using restaurant_server.Persistence;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,49 +18,75 @@ namespace restaurant_server
 {
     class Program
     {
-        static async Task Main(string[] args)
+        class RestaurantApplication : IHostedService
         {
-            using var host = await CreateHostBuilder(args).StartAsync();
-            var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger<Program>();
+            ILoggerFactory _loggerFactory;
+            ILogger<RestaurantApplication> _logger;
+            IServiceProvider _services;
+            IHostApplicationLifetime _lifetime;
+            ConnectionHandler? _connectionHandler;
 
-            logger.LogInformation("Hello World!");
+            public UInt16 Port { get; private set; }
 
-            var dbContext = host.Services.GetRequiredService<RestaurantContext>();
-            await dbContext.Database.MigrateAsync();
-
-            //using (var trx = await dbContext.Database.BeginTransactionAsync())
-            //{
-            //    dbContext.AddRangeAsync(new User { }, new Food { });
-            //}
-
-            var comm = new Communication(new IPEndPoint(IPAddress.Any, 9007));
-            CancellationTokenSource tokenSource = new CancellationTokenSource();
-            CancellationToken _cancellation = tokenSource.Token;
-            Model model = new Model(() => host.Services.GetRequiredService<RestaurantContext>());
-            await using (var connectionHandler = new ConnectionHandler(comm, model, _cancellation, loggerFactory.CreateLogger<ConnectionHandler>()))
+            public RestaurantApplication(IConfiguration configuration, IServiceProvider services, IHostApplicationLifetime lifetime)
             {
-                Console.CancelKeyPress += (s, args) =>
+                _services = services;
+                _loggerFactory = _services.GetRequiredService<ILoggerFactory>();
+                _logger = _loggerFactory.CreateLogger<RestaurantApplication>();
+                _lifetime = lifetime;
+                if (UInt16.TryParse(configuration["port"], out var port))
                 {
-                    tokenSource.Cancel();
-                    args.Cancel = true;
-                };
-                try
-                {
-                    logger.LogInformation("Listening...");
-                    await comm.Listen(_cancellation);
+                    Port = port;
                 }
-                catch (OperationCanceledException)
+                else
                 {
-                    logger.LogInformation("Listen finished");
-                }
-                catch (Exception e)
-                {
-                    logger.LogCritical(e, "Unhandled exception");
+                    _logger.LogCritical("Can not start: {} is not a valid port number", configuration["port"]);
+                    _lifetime.StopApplication();
                 }
             }
 
-            logger.LogInformation("Goodbye cruel world");
+            public async Task StartAsync(CancellationToken cancellationToken)
+            {
+                if (_lifetime.ApplicationStopping.IsCancellationRequested)
+                {
+                    return;
+                }
+                using (var db = _services.GetRequiredService<RestaurantContext>())
+                {
+                    await db.Database.MigrateAsync();
+                }
+
+                var comm = new Communication(new IPEndPoint(IPAddress.Any, Port));
+                var model = new Model(() => _services.GetRequiredService<RestaurantContext>());
+                _connectionHandler = new ConnectionHandler(comm, model, cancellationToken, _loggerFactory.CreateLogger<ConnectionHandler>());
+                try
+                {
+                    _logger.LogInformation("Listening...");
+                    await comm.Listen(cancellationToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    _logger.LogInformation("Listen finished");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogCritical(e, "Unhandled exception");
+                }
+            }
+
+            public async Task StopAsync(CancellationToken cancellationToken)
+            {
+                if (_connectionHandler != null)
+                {
+                    await _connectionHandler.DisposeAsync();
+                }
+                _logger.LogInformation("Goodbye cruel world");
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -64,15 +94,27 @@ namespace restaurant_server
             return Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
                 {
-                    //builder.AddJsonFile("");
-                })
-                .ConfigureServices(services =>
-                {
-                    // services.Configuration;
-                    services.AddDbContext<RestaurantContext>(options =>
+                    builder.AddInMemoryCollection(new Dictionary<string, string>
                     {
-                        options.UseNpgsql("Host=localhost;Port=5432;Database=restaurant;Username=postgres;Password=admin");
-                    }, ServiceLifetime.Transient);
+                        { "port", "9007" },
+                        { "databaseConnectionString", "Host=localhost;Port=5432;Database=restaurant;Username=postgres;Password=admin" }
+                    });
+                    string configBase;
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        configBase = "/etc";
+                    }
+                    else
+                    {
+                        configBase = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData, Environment.SpecialFolderOption.Create);
+                    }
+                    configBase = Path.Join(configBase, "restaurant");
+                    Directory.CreateDirectory(configBase);
+                    builder.SetBasePath(configBase);
+                    builder.AddJsonFile("server.json", optional: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
                     services.AddLogging(builder =>
                     {
                         builder.AddConsole(console =>
@@ -80,8 +122,14 @@ namespace restaurant_server
                             console.IncludeScopes = false;
                             console.TimestampFormat = "[HH:mm:ss] ";
                         });
+                        //builder.AddFile();
                         builder.SetMinimumLevel(LogLevel.Debug);
                     });
+                    services.AddDbContext<RestaurantContext>(options =>
+                    {
+                        options.UseNpgsql(context.Configuration["databaseConnectionString"]);
+                    }, ServiceLifetime.Transient);
+                    services.AddHostedService<RestaurantApplication>();
                 });
         }
     }
